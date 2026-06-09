557bd5b467d517d838578b80d08d4ab09998610b
Eric <eric@uzury.com>
2026-06-09 16:30:38 +0300

    Resize logos to be more prominent

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -56,8 +56,8 @@ export default function Sidebar() {
 
       {/* Logo / Brand */}
       <div className={cn('flex items-center gap-3 px-4 py-5 border-b border-white/[0.06]', collapsed && 'justify-center px-0')}>
-        <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-white p-1 shadow-[0_0_15px_rgba(14,165,233,0.3)] border border-white/20">
-          <img src="/logo.jpg" alt="Uzury Empowerment Hub" className="h-full w-full object-contain" />
+        <div className="flex h-14 w-14 shrink-0 items-center justify-center rounded-xl bg-white p-1.5 shadow-[0_0_15px_rgba(14,165,233,0.3)] border border-white/20">
+          <img src="/logo.jpg" alt="Uzury Empowerment Hub" className="h-full w-full object-contain mix-blend-multiply" />
         </div>
         {!collapsed && (
           <div>
